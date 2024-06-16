fb39adebe52481fec9df6c6312b5e83c5c9b271f
RomanB <djimbik93@gmail.com>
2024-06-16 20:13:26 +0300

    hw-1 done

--- 1-map/map.js
+++ 1-map/map.js
@@ -1,2 +1,13 @@
 //HW - 1 - Map
 
+const arr = [
+    {id: 1, name: 'Вася'},
+    {id: 2, name: 'Петя'},
+    {id: 1, name: 'Вася'},
+]
+
+const ids = new Set(arr.map(item => item.id));
+
+const uniqArr = [...ids].map(item => arr.find(man => man.id === item));
+
+console.log(uniqArr);
